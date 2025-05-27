44f72cb3df8d7edd143a6eee4b9e997eb01d3438
Makinde Mayowa <mayowamakinde23@gmail.com>
2025-05-27 18:37:14 +0100

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
+      '@next/next/no-html-link-for-pages' : 'off',
     },
   }),
 ]
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function NotFound() {
@@ -5,7 +6,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-screen text-center">
         <h1 className="text-6xl font-bold text-gray-800">404</h1>
         <p className="text-2xl mt-4 text-gray-600">Oops! The page you're looking for doesn't exist.</p>
-        <a href="/" className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Go Back Home</a>
+        <Link href="/" className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Go Back Home</Link>
     </div>
   )
 }
